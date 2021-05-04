ba7d4648cf20089d2017d8d88dd03819dbbaca4a
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-05-04 14:11:39 -0400

    edit nodejs

--- app.js
+++ app.js
@@ -4,27 +4,22 @@ require('dotenv').config()
 let mongodb = require('mongodb');
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
+const bcrypt = require('bcryptjs');
+//const passport = require('passport');
+//const flash = require('express-flash');
+//const session = require('express-session');
 
 app.listen(3000, () => {
     console.log(`App listening on port ${3000}`);
 });
 
 // loading static files
-// adapted from: https://www.youtube.com/watch?v=A01KtJTv1oc&ab_channel=RaddyTheBrand
-//app.use(express.static(__dirname +"/public"));
+// adapted from: https://expressjs.com/en/starter/static-files.html [5/3/2021]
 var path = require('path');
 app.use(express.static(path.join(__dirname, '/public')));
 
-// set views
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'html');
-
 // database information
-mongoose.connect("mongodb+srv://marinos:open123@cluster0.7pdeb.mongodb.net/Cluster0?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
+mongoose.connect("mongodb+srv://marinos:open123@cluster0.7pdeb.mongodb.net/Cluster0?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true });
 
 // create schema
 const { Schema } = mongoose;
@@ -39,13 +34,12 @@ const CLASS  = new Schema({
     capacity: {type: Number}
 });
 
-// schema for new users
+// schema for new Users
 const NEWUSER = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     userName: {type: String, required: true},
     password: {type: String, required: true},
-    passwordConfirm: {type: String, required: true},
     userType: {type: String, required: true},
     creationDate: {type: Date, default: Date.now},
     classes: [CLASS]
@@ -57,113 +51,138 @@ let classCreate = mongoose.model("classCreate", CLASS);
 
 // GET REQUESTS
 app.get('/', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/index.html");
-
 });
 
 app.get('/index.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/index.html");
-
 });
 
 app.get('/login-page-html.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/login-page-html.html");
 });
 
 app.get('/forgot-password-html.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/forgot-password-html.html");
 });
 
 app.get('/create-account-page.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/create-account-page.html");
 });
 
 app.get('/add-class.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/add-class.html");
 });
 
 app.get('/change-password-instructor.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/change-password-instructor.html");
 });
 
 app.get('/change-password-student.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/change-password-student.html");
 });
 
 app.get('/class-deadline-instructor.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/class-deadline-instructor.html");
 });
 
 app.get('/class-deadline-student.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/class-deadline-student.html");
 });
 
 app.get('/create-class.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/create-class.html");
 });
 
 app.get('/delete-class.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/delete-class.html");
 });
 
 app.get('/drop-class.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/drop-class.html");
 });
 
 app.get('/instructor-course-dictionary-html.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/instructor-course-dictionary-html.html");
 });
 
 app.get('/student-course-dictionary-html.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/student-course-dictionary-html.html");
 });
 
 app.get('/instructor-dashboard-html.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/instructor-dashboard-html.html");
 });
 
 app.get('/student-dashboard-html.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/student-dashboard-html.html");
 });
 
 app.get('/shopping-cart.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/shopping-cart.html");
 });
 
 app.get('/create-class.html', (req, res) => {
-    // load html file
     res.sendFile(__dirname + "/views/html/create-class.html");
 });
 
-// adapted from [5/3/2021]: https://www.youtube.com/watch?v=-RCnNyD0L-s&ab_channel=WebDevSimplified
+// adapted from [5/4/2021]: https://www.youtube.com/watch?v=Ud5xKCYQTjM&ab_channel=WebDevSimplified
 // create a post request for the login page
-app.post("/login-page.html", bodyParser.urlencoded({extended: false}), async function (req, res) {
+app.post("/login-page-html.html", bodyParser.urlencoded({extended: false}), async function (req, res) {
 
+    let entered_username = req.body.username;
+    let entered_password = req.body.password;
+
+    console.log(req.body);
+
+    // get user with same name from database
+    userCreate.findOne({userName: entered_username}, async function(err, foundUser){
+        if(err) return console.log(err);
+
+        // if the user is correct then check to see if passwords match
+        if (foundUser) {
+
+            console.log("user found");
+
+            // comparing passwords
+            try {
+
+                if (await bcrypt.compare(entered_password, foundUser.password)) {
+                    // check to see if the user is a student or instructor and then direct them to the appropriate page
+                    let usertype = foundUser.userType;
+
+                    // Student Case
+                    if (usertype == "Student") {
+                        console.log("login sucess to student");
+                        res.redirect("/student-dashboard-html.html");
+                    }
+
+                    // Instructor Case
+                    if (usertype == "Instructor") {
+                        console.log("login sucess to instructor");
+                        res.redirect("/instructor-dashboard-html.html")
+                    }
+
+                } else {
+                    // display message "Incorrect username or password"
+                    console.log("wrong username of password");
+                }
+
+            } catch (e) {console.log(e)};
+
+        } // end if foundUser
+
+        // if the user is not in the database
+        else {
+            console.log("This account does not exist, please create an account to log in")
+        }
+    });
 });
 
 // create a post request for the create account page
 app.post("/create-account-page.html", bodyParser.urlencoded({extended: false}), async function (req, res) {
 
     // check to see if username and password are suitable
-
 
     try {
         // hashing password
@@ -178,9 +197,11 @@ app.post("/create-account-page.html", bodyParser.urlencoded({extended: false}), 
         let userType = req.body["user-type"];
 
         // see if user is already in database, otherwise display error and create a new user
-        userCreate.findOne({username: username}, function(err, foundUser){
+        userCreate.findOne({username: username}, function(err, foundUser) {
             if(err) return res.json({error: "error occurred"});
             if (foundUser) return res.json({error:"Username already taken"});
+
+            // if no error or user found then create one and save to the database
             else {
 
                 // add attributes to new user
@@ -200,6 +221,7 @@ app.post("/create-account-page.html", bodyParser.urlencoded({extended: false}), 
                     console.log("user saved");
                     res.redirect("/login-page-html.html")
                 });
+
             }
         });
 
