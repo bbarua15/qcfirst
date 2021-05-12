8937a0d819824d06cbcc02bfffbc2bd40a1153e2
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-05-12 17:54:46 -0400

    fixed user auth to pages

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,35 @@
-// adapted from: https://www.youtube.com/watch?v=6FOq4cUdH8k&ab_channel=TraversyMedia
+// adapted from: https://www.youtube.com/watch?v=6FOq4cUdH8k&ab_channel=TraversyMedia, https://stackify.com/node-js-module-exports/
 
 // middleware to make sure that a user has permission to access a specific route
-module.exports = {
-  ensureAuthenticated: function(req, res, next) {
-    if(req.isAuthenticated()) {
+
+function ensureAuthenticatedStudent(req, res, next) {
+  if (req.user != "undefined") {
+    if(req.isAuthenticated() && req.user.userType == "Student") {
       return next();
     }
-    req.flash("error_msg", "Access denied! Please log in to view this page");
-    res.redirect("/users/login");
   }
+  req.flash("error_msg", "Access denied! Please log in to view this page");
+  res.redirect("/users/login");
 }
+
+function ensureAuthenticatedInstructor(req, res, next) {
+  if (req.user != "undefined") {
+    if(req.isAuthenticated() && req.user.userType == "Instructor") {
+      return next();
+    }
+  }
+  req.flash("error_msg", "Access denied! Please log in to view this page");
+  res.redirect("/users/login");
+}
+
+function ensureAuthenticatedAdmin(req, res, next) {
+  if (req.user != "undefined") {
+    if(req.isAuthenticated() && req.user.userType == "Admin") {
+      return next();
+    }
+  }
+  req.flash("error_msg", "Access denied! Please log in to view this page");
+  res.redirect("/users/login");
+}
+
+module.exports = {ensureAuthenticatedStudent, ensureAuthenticatedInstructor, ensureAuthenticatedAdmin}
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const { ensureAuthenticatedStudent } = require("../config/authStudent");
-const { ensureAuthenticatedInstructor } = require("../config/authInstructor");
-const { ensureAuthenticatedAdmin } = require("../config/authAdmin");
-const { ensureAuthenticated } = require("../config/auth");
+const { ensureAuthenticatedStudent, ensureAuthenticatedInstructor, ensureAuthenticatedAdmin } = require("../config/auth");
 
 // home page
 router.get("/", (req, res) => res.render("index"));
@@ -15,47 +12,56 @@ router.get("/", (req, res) => res.render("index"));
 // STUDENT PAGES
 
 // student dashboard
-router.get("/student-dashboard", ensureAuthenticated, (req, res) => res.render("student-dashboard-html"));
+router.get("/student-dashboard", ensureAuthenticatedStudent, (req, res, next) => {
+    res.render("student-dashboard-html");
+});
 
 // change password student
-router.get("/change-password-student", ensureAuthenticated, (req, res) => res.render("change-password-student"));
+router.get("/change-password-student", ensureAuthenticatedStudent, (req, res) => res.render("change-password-student"));
 
 // class deadline student
-router.get("/class-deadline-student", ensureAuthenticated, (req, res) => res.render("class-deadline-student"));
+router.get("/class-deadline-student", ensureAuthenticatedStudent, (req, res) => res.render("class-deadline-student"));
 
 // add class
-router.get("/add-class", ensureAuthenticated, (req, res) => res.render("add-class"));
+router.get("/add-class", ensureAuthenticatedStudent, (req, res) => res.render("add-class"));
 
 // drop class
-router.get("/drop-class", ensureAuthenticated, (req, res) => res.render("drop-class"));
+router.get("/drop-class", ensureAuthenticatedStudent, (req, res) => res.render("drop-class"));
 
 // shopping cart
-router.get("/shopping-cart", ensureAuthenticated, (req, res) => res.render("shopping-cart"));
+router.get("/shopping-cart", ensureAuthenticatedStudent, (req, res) => res.render("shopping-cart"));
 
 // student course dictionary
-router.get("/student-course-dictionary", ensureAuthenticated, (req, res) => res.render("student-course-dictionary-html"));
+router.get("/student-course-dictionary", ensureAuthenticatedStudent, (req, res) => res.render("student-course-dictionary-html"));
 
 /*=======================================================*/
 
 // INSTRUCTOR PAGES
 
 // instructor dashboard
-router.get("/instructor-dashboard", ensureAuthenticated, (req, res) => res.render("instructor-dashboard-html"));
+router.get("/instructor-dashboard", ensureAuthenticatedInstructor, (req, res) => res.render("instructor-dashboard-html"));
 
 // change password instructor
-router.get("/change-password-instructor", ensureAuthenticated, (req, res) => res.render("change-password-instructor"));
+router.get("/change-password-instructor", ensureAuthenticatedInstructor, (req, res) => res.render("change-password-instructor"));
 
 // class deadline instructor
-router.get("/class-deadline-instructor", ensureAuthenticated, (req, res) => res.render("class-deadline-instructor"));
+router.get("/class-deadline-instructor", ensureAuthenticatedInstructor, (req, res) => res.render("class-deadline-instructor"));
 
 // create class
-router.get("/create-class", ensureAuthenticated, (req, res) => res.render("create-class"));
+router.get("/create-class", ensureAuthenticatedInstructor, (req, res) => res.render("create-class"));
 
 // delete class
-router.get("/delete-class", ensureAuthenticated, (req, res) => res.render("delete-class"));
+router.get("/delete-class", ensureAuthenticatedInstructor, (req, res) => res.render("delete-class"));
 
 // instructor course dictionary
-router.get("/instructor-course-dictionary", ensureAuthenticated, (req, res) => res.render("instructor-course-dictionary-html"));
+router.get("/instructor-course-dictionary", ensureAuthenticatedInstructor, (req, res) => res.render("instructor-course-dictionary-html"));
+
+/*=======================================================*/
+
+// ADMIN PAGES
+
+// admin dashboard
+// TBA
 
 /*=======================================================*/
 
