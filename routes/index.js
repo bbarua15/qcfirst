422bc714988ca0009e12f61562c611e117d26830
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-05-12 22:14:07 -0400

    edited footer paths

--- routes/index.js
+++ routes/index.js
@@ -13,48 +13,72 @@ router.get("/", (req, res) => res.render("index"));
 
 // student dashboard
 router.get("/student-dashboard", ensureAuthenticatedStudent, (req, res, next) => {
-    res.render("student-dashboard-html");
+    res.render("student-dashboard-html",{firstName: req.user.firstName, lastName: req.user.lastName});
 });
 
 // change password student
-router.get("/change-password-student", ensureAuthenticatedStudent, (req, res) => res.render("change-password-student"));
+router.get("/change-password-student", ensureAuthenticatedStudent, (req, res) => {
+    res.render("change-password-student",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // class deadline student
-router.get("/class-deadline-student", ensureAuthenticatedStudent, (req, res) => res.render("class-deadline-student"));
+router.get("/class-deadline-student", ensureAuthenticatedStudent, (req, res) =>  {
+    res.render("class-deadline-student",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // add class
-router.get("/add-class", ensureAuthenticatedStudent, (req, res) => res.render("add-class"));
+router.get("/add-class", ensureAuthenticatedStudent, (req, res) => {
+    res.render("add-class",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // drop class
-router.get("/drop-class", ensureAuthenticatedStudent, (req, res) => res.render("drop-class"));
+router.get("/drop-class", ensureAuthenticatedStudent, (req, res) => {
+    res.render("drop-class",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // shopping cart
-router.get("/shopping-cart", ensureAuthenticatedStudent, (req, res) => res.render("shopping-cart"));
+router.get("/shopping-cart", ensureAuthenticatedStudent, (req, res) =>  {
+    res.render("shopping-cart",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // student course dictionary
-router.get("/student-course-dictionary", ensureAuthenticatedStudent, (req, res) => res.render("student-course-dictionary-html"));
+router.get("/student-course-dictionary", ensureAuthenticatedStudent, (req, res) =>  {
+    res.render("student-course-dictionary-html",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 /*=======================================================*/
 
 // INSTRUCTOR PAGES
 
 // instructor dashboard
-router.get("/instructor-dashboard", ensureAuthenticatedInstructor, (req, res) => res.render("instructor-dashboard-html"));
+router.get("/instructor-dashboard", ensureAuthenticatedInstructor, (req, res) =>  {
+    res.render("instructor-dashboard-html",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // change password instructor
-router.get("/change-password-instructor", ensureAuthenticatedInstructor, (req, res) => res.render("change-password-instructor"));
+router.get("/change-password-instructor", ensureAuthenticatedInstructor, (req, res) =>  {
+    res.render("change-password-instructor",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // class deadline instructor
-router.get("/class-deadline-instructor", ensureAuthenticatedInstructor, (req, res) => res.render("class-deadline-instructor"));
+router.get("/class-deadline-instructor", ensureAuthenticatedInstructor, (req, res) =>  {
+    res.render("class-deadline-instructor",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // create class
-router.get("/create-class", ensureAuthenticatedInstructor, (req, res) => res.render("create-class"));
+router.get("/create-class", ensureAuthenticatedInstructor, (req, res) =>  {
+    res.render("create-class",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // delete class
-router.get("/delete-class", ensureAuthenticatedInstructor, (req, res) => res.render("delete-class"));
+router.get("/delete-class", ensureAuthenticatedInstructor, (req, res) =>  {
+    res.render("delete-class",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 // instructor course dictionary
-router.get("/instructor-course-dictionary", ensureAuthenticatedInstructor, (req, res) => res.render("instructor-course-dictionary-html"));
+router.get("/instructor-course-dictionary", ensureAuthenticatedInstructor, (req, res) =>  {
+    res.render("instructor-course-dictionary-html",{firstName: req.user.firstName, lastName: req.user.lastName})
+});
 
 /*=======================================================*/
 
