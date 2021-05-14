702cb79421dd03e746cbd16e0e0dddaafb8206b9
bbarua15 <70711478+bbarua15@users.noreply.github.com>
2021-05-13 21:46:36 -0400

    display classes

--- routes/index.js
+++ routes/index.js
@@ -81,7 +81,11 @@ router.get("/delete-class", ensureAuthenticatedInstructor, (req, res) =>  {
 
 // instructor course dictionary
 router.get("/instructor-course-dictionary", ensureAuthenticatedInstructor, (req, res) =>  {
-    res.render("instructor-course-dictionary",{firstName: req.user.firstName, lastName: req.user.lastName})
+  classCreate.find({}, function(err, classes) {
+    res.render("instructor-course-dictionary", {firstName: req.user.firstName, lastName: req.user.lastName,
+       classList: classes
+      }) 
+  })
 });
 
 /*=======================================================*/
