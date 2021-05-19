8903b7cb088aee018a502247c41abaaead97d4dd
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-05-18 23:07:26 -0400

    added history schema to store search results

---
+++ models/userHistory.js
@@ -0,0 +1,31 @@
+// adapted from [5/3/2021]: https://mongoosejs.com/docs/schematypes.html
+
+const mongoose = require('mongoose');
+
+// create schema
+const { Schema } = mongoose;
+
+// schema for new Classes
+const CLASS  = new Schema({
+    courseNumber: {type: String},
+    semester: {type: String,},
+    courseName: {type: String},
+    department: {type: String},
+    instructor: {type: String},
+    rosterStudent: {type: [String]},
+    description: {type: String},
+    schedule: {type: String},
+    capacity: {type: Number},
+    startDate: {type: Date, default: Date.now}
+});
+
+// schema for new Users
+const HISTORY = new Schema({
+    history: {type: String},
+    results: [CLASS]
+});
+
+// create model
+let userHistory = mongoose.model("userHistory", HISTORY);
+
+module.exports = userHistory;
